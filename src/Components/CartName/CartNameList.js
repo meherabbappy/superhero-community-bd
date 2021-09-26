8626e4d64150c6d43d023f0b9a725ec3dc16789c
Meherab Hossain <60975003+meherabbappy@users.noreply.github.com>
2021-09-26 21:11:45 +0600

    added

--- src/Components/CartName/CartNameList.js
+++ src/Components/CartName/CartNameList.js
@@ -2,10 +2,22 @@ import React from 'react';
 import './CartNameList.css'
 
 const CartNameList = (props) => {
-    const {name} = props.persons;
+    const {name,img} = props.persons;
     return (
-        <div>
-            <h1 className = 'list-name'>{name}</h1>
+        <div className = 'list-cart'>
+            <div class="card mb-3">
+                <div class="row g-0">
+                <div class="col-md-4">
+                    <img className = 'cart-img' src={img} alt="" />
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                    <h5 class="cart-name">{name}</h5>
+                    
+                    </div>
+                </div>
+                </div>
+                </div>
         </div>
     );
 };
--- src/Components/CartTotal/CartTotal.js
+++ src/Components/CartTotal/CartTotal.js
@@ -9,8 +9,8 @@ const CartTotal = (props) => {
     }
     return (
         <div className = 'cart-total'>
-             <h3><i class="fas fa-shopping-cart"></i> Total Added: {props.carts.length}</h3>
-            <h3><i class="fas fa-money-check-alt"></i> Income status: {total} </h3>
+             <h3 className = 'name1'><i class="fas fa-shopping-cart"></i> Total Added: {props.carts.length}</h3>
+            <h3 className = 'name1'><i class="fas fa-money-check-alt"></i> Income status: {total} </h3>
         </div>
     );
 };
