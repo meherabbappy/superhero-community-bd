0091a93bed6bb7b05f19fd0c7f2e3fe892261a3a
Meherab Hossain <60975003+meherabbappy@users.noreply.github.com>
2021-09-26 14:45:16 +0600

    added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -13,17 +13,6 @@ const Header = () => {
     );
 };
 
-// const youtubers = [
-//     {name:'Hero Alam',age:32,nationality:"Bangladesh",content:"Actor",income:"100",img:""},
-//     {name:'Salman Muktadir',age:26,nationality:"Bangladesh",content:"Funny",income:"150",img:""},
-//     {name:'Towhid Afridi',age:25,nationality:"Bangladesh",content:"Vlog",income:"250",img:""},
-//     {name:'Petuk Couple',age:29,nationality:"Bangladesh",content:"Food Vlog",income:"180",img:""},
-//     {name:'Khuda Lagce',age:27,nationality:"Bangladesh",content:"Food Vlog",income:"200",img:""},
-//     {name:'Ayman Sadik',age:26,nationality:"Bangladesh",content:"Educational",income:"350",img:""},
-//     {name:'Solaiman Sukhon',age:40,nationality:"Bangladesh",content:"Motivational",income:"300",img:""},
-//     {name:'Programming Hero',age:26,nationality:"Bangladesh",content:"Programming",income:"400",img:""},
-//     {name:'Ashraf Niloy',age:28,nationality:"Bangladesh",content:"Fashion",income:"350",img:""},
-// ]
 
 
 export default Header;
